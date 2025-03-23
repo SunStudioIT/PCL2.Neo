1a022bdaff0d62f9f37f6f99335e6a6d569f90c7
Hill23333 <a18035135032@163.com>
2025-03-23 13:41:55 +0800

    feat: custom scroll bar

---
+++ PCL2.Neo/Controls/MyMsg/MyMsgText.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using PCL2.Neo.Helpers;
+
+namespace PCL2.Neo.Controls.MyMsg
+{
+    public class MyMsgText : TemplatedControl , IMessageBox
+    {
+        public MyMsgText(MessageBoxParam param)
+        {
+
+        }
+    }
+}
